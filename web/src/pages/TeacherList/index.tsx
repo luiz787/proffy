9c601ccb84a6a3ecd5091c3a49f9417aa22327c5
Luiz Berto <diasbertoluiz@gmail.com>
2020-08-03 15:06:37 -0300

    Creates TeacherList header

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,9 +1,30 @@
 import React, { FunctionComponent } from "react";
+import { Link } from "react-router-dom";
+
+import logoImg from "../../assets/images/logo.svg";
+import backIcon from "../../assets/images/icons/back.svg";
+
+import "./styles.css";
 
 export interface TeacherListProps {}
 
 const TeacherList: FunctionComponent<TeacherListProps> = () => {
-  return <h1>TeacherList</h1>;
+  return (
+    <div id="page-teacher-list" className="container">
+      <header className="page-header">
+        <div className="top-bar-container">
+          <Link to="/">
+            <img src={backIcon} alt="Voltar" />
+          </Link>
+          <img src={logoImg} alt="Proffy" />
+        </div>
+
+        <div className="header-content">
+          <strong>Estes são os proffys disponíveis.</strong>
+        </div>
+      </header>
+    </div>
+  );
 };
 
 export default TeacherList;
