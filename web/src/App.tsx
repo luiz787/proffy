88e6855f7bb0f0603eb4667870a1e6e10724b054
Luiz Berto <diasbertoluiz@gmail.com>
2020-08-03 14:45:36 -0300

    Adds landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 
+import "./assets/styles/global.css";
+import LandingPage from "./pages/Landing";
+
 function App() {
-  return <h1>Hello, World!</h1>;
+  return <LandingPage />;
 }
 
 export default App;
