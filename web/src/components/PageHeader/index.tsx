aad442fde5050cc77666d1f03953de0448cba85d
Luiz Berto <diasbertoluiz@gmail.com>
2020-08-03 16:01:23 -0300

    Styling of TeacherList and TeacherItem

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -4,11 +4,13 @@ import { Link } from "react-router-dom";
 import logoImg from "../../assets/images/logo.svg";
 import backIcon from "../../assets/images/icons/back.svg";
 
+import "./styles.css";
+
 export interface PageHeaderProps {
   title: string;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({ title, children }) => {
   return (
     <header className="page-header">
       <div className="top-bar-container">
@@ -20,6 +22,7 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
 
       <div className="header-content">
         <strong>{title}</strong>
+        {children}
       </div>
     </header>
   );
--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -6,7 +6,7 @@ export interface TeacherFormProps {}
 const TeacherForm: FunctionComponent<TeacherFormProps> = () => {
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="Que bom que você quer dar aulas!" />
+      <PageHeader title="Que incrível que você quer dar aulas." />
     </div>
   );
 };
